500f1f94e5df89f4a43ee1848c793e751ed46ef0
rohitg-city <Rohit.Gurunathan@city.ac.uk>
2025-08-22 14:24:47 +0100

    Registrar to register routes to a Gin router instance

---
+++ routes/route-registrar.go
@@ -0,0 +1,10 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+// RouteRegistrar This function can be used to register routes defined in this package to a Gin router instance.
+func RouteRegistrar(server *gin.Engine) {
+	server.GET("/events", getEvents)          // Register the GET request to /events with the getEvents function to retrieve all events.
+	server.GET("/events/:id", getSingleEvent) //Using :id can allow for dynamic ID parameters in the URL.
+	server.POST("/events", createEvent)       // Register the POST request to /events with the createEvent function to create a new event.
+}
