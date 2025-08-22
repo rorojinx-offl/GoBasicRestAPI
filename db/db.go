819ff23af7458122e8250ed900732f8561677810
rohitg-city <Rohit.Gurunathan@city.ac.uk>
2025-08-22 19:15:44 +0100

    Query for new table

--- db/db.go
+++ db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 	_ "github.com/mattn/go-sqlite3" //Import with underscore so that we don't use it directly, but we have SQL lib use its functionalities
 )
 
@@ -22,6 +23,18 @@ func InitDB() {
 }
 
 func createTables() {
+	createUserTable := `
+	CREATE TABLE IF NOT EXISTS users (
+	    	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	    	email TEXT NOT NULL UNIQUE,
+	    	password TEXT NOT NULL
+	)
+`
+	_, err := DB.Exec(createUserTable)
+	if err != nil {
+		panic(fmt.Sprintf("Could not configure table! %v", err))
+	}
+
 	createEventsTable := `
 	CREATE TABLE IF NOT EXISTS events (
 	    id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -29,11 +42,13 @@ func createTables() {
 	    description TEXT NOT NULL,
 	    location TEXT NOT NULL,
 	    dateTime DATETIME NOT NULL,
-	    user_id INTEGER
+	    user_id INTEGER,
+	    FOREIGN KEY(user_id) REFERENCES users(id)
 	)
 	`
-	_, err := DB.Exec(createEventsTable)
+	_, err = DB.Exec(createEventsTable)
 	if err != nil {
-		panic("Could not configure table!")
+		panic(fmt.Sprintf("Could not configure table! %v", err))
 	}
+
 }
