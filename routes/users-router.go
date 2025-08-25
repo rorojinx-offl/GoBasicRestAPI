18fb41df28d10eaa46d2a417a87a51cc29b12e66
rohitg-city <Rohit.Gurunathan@city.ac.uk>
2025-08-25 17:33:47 +0100

    new function to route login input to credential verification

--- routes/users-router.go
+++ routes/users-router.go
@@ -22,3 +22,19 @@ func signup(context *gin.Context) {
 	}
 	context.JSON(http.StatusCreated, gin.H{"Success": "User created successfully"})
 }
+
+func login(context *gin.Context) {
+	user := models.User{}
+
+	err := context.ShouldBindJSON(&user)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"Error": "Could not parse request"})
+	}
+
+	err = user.ValidateCreds()
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"Error": "Invalid credentials"})
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{"Success": "Login successful"})
+}
