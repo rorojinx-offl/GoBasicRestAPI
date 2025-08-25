b359d16ebfee3bffa4296e0fceed19b6b6133f59
rohitg-city <Rohit.Gurunathan@city.ac.uk>
2025-08-25 17:32:18 +0100

    new function to compare inputted plaintext password and stored hashes

--- utils/hash.go
+++ utils/hash.go
@@ -6,3 +6,8 @@ func HashPassword(plaintext string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(plaintext), 14) //Generate hased password from a byte-casted plaintext using bcrypt with a cost of 14.
 	return string(bytes), err                                        //Return the hashed password as a string.
 }
+
+func CheckPasswordHash(plaintext, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(plaintext)) //Compare a hashed password with its possible plaintext equivalent.
+	return err == nil                                                     //Return true if they match, false otherwise.
+}
