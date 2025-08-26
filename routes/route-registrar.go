f87f185d1cdd841c479e53b4d7e35c39e4e2d3aa
rohitg-city <Rohit.Gurunathan@city.ac.uk>
2025-08-26 14:08:57 +0100

    Route registrar rearranged to protect certain routes by grouping them together and applying the Authentication middleware to them

--- routes/route-registrar.go
+++ routes/route-registrar.go
@@ -1,15 +1,22 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"GoBasicRestAPI/utils"
+	"github.com/gin-gonic/gin"
+)
 
 // RouteRegistrar This function can be used to register routes defined in this package to a Gin router instance.
 func RouteRegistrar(server *gin.Engine) {
 	//Event routes
 	server.GET("/events", getEvents)          // Register the GET request to /events with the getEvents function to retrieve all events.
 	server.GET("/events/:id", getSingleEvent) //Using :id can allow for dynamic ID parameters in the URL to get individual events.
-	server.POST("/events", createEvent)       // Register the POST request to /events with the createEvent function to create a new event.
-	server.PUT("/events/:id", updateEvent)    // Register the PUT request to /events/:id with the updateEvent function to update an existing event.
-	server.DELETE("/events/:id", deleteEvent) // Register the DELETE request to /events/:id with the deleteEvent function to delete an event.
+
+	//Protected routes - only authenticated users can create events
+	protected := server.Group("/")               // Create a route group for protected routes
+	protected.Use(utils.Authenticate)            // Apply the authentication middleware to the group
+	protected.POST("/events", createEvent)       // Register the POST request to /events with the auth middleware first and then createEvent function to create a new event.
+	protected.PUT("/events/:id", updateEvent)    // Register the PUT request to /events/:id with the updateEvent function to update an existing event.
+	protected.DELETE("/events/:id", deleteEvent) // Register the DELETE request to /events/:id with the deleteEvent function to delete an event.
 
 	//User routes
 	server.POST("/signup", signup) // Register the POST request to /signup with the signup function to create a new user.
